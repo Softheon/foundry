cecfd5d6e892ac8502f271465e835f2972a67f31
SoftheonHXia <40275617+SoftheonHXia@users.noreply.github.com>
2019-06-14 14:56:43 -0400

    add DashboardCrossfilterControl component

--- frontend/src/metabase/dashboard/hoc/DashboardCrossfilterControl.jsx
+++ frontend/src/metabase/dashboard/hoc/DashboardCrossfilterControl.jsx
@@ -1,103 +1,10 @@
-// import React, { Component } from "react";
-// import { connect } from "react-redux";
-// import crossfilter from "crossfilter";
-
-// /**
-//  *  Crossfilter
-//  *   {
-//  *
-//  *    crossfilterId: {
-//  *          crossfitler:
-//  *          dimensions: {
-//  *            dimensonName: dimension
-//  *              }
-//  *        }
-//  *
-//  *
-//  * }
-//  *
-//  */
-
-// export default (ComposedComponent) =>
-//   connect(null, null)(
-//     class extends Component {
-//       static displayName = "DashboardCrossFilterControls[" +
-//         (ComposedComponent.displayName || ComposedComponent.name) +
-//         "]";
-
-//         constructor(props) {
-//           super(props);
-//           this.state({
-//             crossfilters: {}
-//           })
-//         }
-
-//         componentWillMount() {
-//             this.initializeCrossfilters();
-//         }
-//         componentWillUnmount() {}
-//         componentDidUpdate() {
-//             this.updateCrossfilters();
-//         }
-
-//         initializeCrossfilters () {
-//           const cfParameters = this.getCrossfilterParameters();
-//           const oldCrossfilters = this.state.crossfitlers;
-//           const newCrossfilters = {
-//             ...oldCrossfilters
-//           }
-//           cfParameters.map(cfParameter => {
-//             const data = this.normalizeCardData(cfParameter);
-//             const crossfilter = crossfilter(data);
-
-//           })
-
-//         }
-
-//         getCrossfilterParameters () {
-//           const { dashboard } = this.props;
-//           const { parameters } = dashboard;
-//           return parameters.filter(parameter => {
-//             return parameter.type && parameter.type === "crossfilter";
-//           })
-//         }
-
-//         updateCrossfilters(){
-
-//         }
-
-//         // normalize a card data to an array of record thats can be understood by crossfilter
-//         normalizeCardData(crossfilterParamter) {
-
-//         }
-
-//         selectedDimensionValues(crossfilterParameter) {
-
-//         }
-//         render () {
-//           console.log("xia: props [DashboardCrossfilterControl]", this.props);
-//             return (
-//                 <ComposedComponent
-//                  {...this.props}
-//                  {...this.state}
-//                 />
-//             )
-//         }
-//     },
-//   );
-
 import React, { Component } from "react";
-
+import crossfilter from "crossfilter";
 import { connect } from "react-redux";
 import { replace } from "react-router-redux";
 
-import MetabaseAnalytics from "metabase/lib/analytics";
-import { parseHashOptions, stringifyHashOptions } from "metabase/lib/browser";
-
-import screenfull from "screenfull";
 import { getIn } from "icepick";
 import type { LocationDescriptor } from "metabase/meta/types";
-import crossfilter from "crossfilter";
 
 /* This contains some state for dashboard controls on both private and embedded dashboards.
  * It should probably be in Redux?
@@ -113,18 +20,30 @@ export default (ComposedComponent: ReactClass<any>) =>
         crossfilters: null,
       };
 
+      componentDidMount() {
+        // this.initializeCrossfilters();
+        this.initializeCrossfilters();
+      }
+
+      componentDidUpdate() {
+        this.updateCrossfilters();
+      }
+
       initializeCrossfilters() {
+
+
         const cfParameters = this.getCrossfilterParameters();
         const oldCrossfilters = this.state.crossfitlers;
+        console.log("xia: cfParameters", cfParameters);
 
-      // remove any inactive crossfilters;
+        // remove any inactive crossfilters;
         const cfParameterIdSet = new Set();
-        
+
         cfParameters.map(cfParameter => {
           cfParameterIdSet.add(cfParameter.card_id);
-        })
+        });
 
-        for(const crossfilterId in oldCrossfilters) {
+        for (const crossfilterId in oldCrossfilters) {
           if (!cfParameterIdSet.has(crossfilterId)) {
             delete oldCrossfilters[crossfilterId];
           }
@@ -133,40 +52,46 @@ export default (ComposedComponent: ReactClass<any>) =>
         const newCrossfilters = {
           ...oldCrossfilters,
         };
+
         // initialize any new crossfilter
         cfParameters.map(cfParameter => {
-          const { card_id, dashcard_id }  = cfParameter;
-          if(!newCrossfilters[card_id]) {
+          const { card_id, dashcard_id } = cfParameter;
+          if (!newCrossfilters[card_id]) {
             const data = this.normalizeCardData(cfParameter);
-            const crossfilter = crossfilter(data);
+            const crossfilterObject = crossfilter(data);
             newCrossfilters[card_id] = {
-              crossfilter: crossfilter,
-              dimensions: {}
-            }
+              crossfilter: crossfilterObject,
+              dimensions: {},
+              card_id: card_id,
+              dashcard_id: dashcard_id,
+              updateAt: new Date(),
+            };
           }
           const currentCrossfilter = newCrossfilters[card_id];
-          const selectedDimensionValues  = selectedDimensionValues(cfParameter);
+          const selectedDimensionValues = this.selectedDimensionValues(cfParameter);
           const dimensionValueSet = new Set(selectedDimensionValues);
           const dimensions = currentCrossfilter.dimensions;
           // dispose inactive dimensions
-          for(const dimensionName in dimensions) {
-              if (!dimensionValueSet.has(dimensionName)){
-                  dimensions[dimensionName].dispose();
-                  delete dimensions[dimensionName];
-              }
+          for (const dimensionName in dimensions) {
+            if (!dimensionValueSet.has(dimensionName)) {
+              dimensions[dimensionName].dispose();
+              delete dimensions[dimensionName];
+            }
           }
           // add new dimensions
           const crossfilterObject = currentCrossfilter.crossfilter;
-          for(const dimensionName of selectedDimensionValues) {
+          for (const dimensionName of selectedDimensionValues) {
             if (!dimensions[dimensionName]) {
-                dimensions[dimensionName] = crossfilterObject.dimension(
-                  d => {
-                    return d[dimensionName];
-                  }
-                )
+              dimensions[dimensionName] = crossfilterObject.dimension(d => {
+                return d[dimensionName];
+              });
             }
           }
         });
+
+        this.setState({
+          crossfilters: newCrossfilters
+        })
       }
 
       getCrossfilterParameters() {
@@ -179,33 +104,48 @@ export default (ComposedComponent: ReactClass<any>) =>
 
       updateCrossfilters() {}
 
+      isLoading() {
+      
+      }
       // normalize a card data to an array of record thats can be understood by crossfilter
       normalizeCardData(crossfilterParamter) {
-        const {card_id, dashcard_id } = crossfilterParamter;
+        const { card_id, dashcard_id } = crossfilterParamter;
         const { dashcardData } = this.props;
         const data = getIn(dashcardData, [dashcard_id, card_id, "data"]);
-        const {cols , columns, rows } = data;
+        console.log("xia: all properties", this.props);
+        console.log("xia: normalize crossfilter parameter", crossfilterParamter);
+        console.log("xia: all dashcard car data", dashcardData);
+        console.log("xia: dash card data", data);
+        // if (!data) {
+        //   return [];
+        // }
+  
+        const { cols, columns, rows } = data;
         const result = [];
         const rowCount = rows.length;
         const colCount = cols.length;
         for (let i = 0; i < rowCount; i++) {
-            const normalizedRow = {};
-            const currentRow = rows[i];
-            for(let j = 0; j < colCount; j++) {
-              normalizedRow[columns[j]] = currentRow[j];
-            }
-            result.push(normalizedRow);
+          const normalizedRow = {};
+          const currentRow = rows[i];
+          for (let j = 0; j < colCount; j++) {
+            normalizedRow[columns[j]] = currentRow[j];
+          }
+          result.push(normalizedRow);
         }
         return result;
       }
 
       selectedDimensionValues(crossfilterParameter) {
-        
+        const { parameterValues } = this.props;
+        const parameterId = crossfilterParameter.id;
+        let dimensionValues = parameterValues[parameterId];
+        if (!Array.isArray(dimensionValues)) {
+          dimensionValues = [dimensionValues];
+        }
+        return dimensionValues ? dimensionValues : [];
       }
 
-      
       render() {
-        console.log("xia: props [DashboardCrossfilterControl]", this.props);
         return <ComposedComponent {...this.props} {...this.state} />;
       }
     },
